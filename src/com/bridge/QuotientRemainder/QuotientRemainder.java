cf175384789548e36b10125e26bd7123ba2b1bed
Navneet <navn0306@gmail.com>
2021-11-28 00:36:41 +0530

    Add: Added welcome message to program

---
+++ src/com/bridge/QuotientRemainder/QuotientRemainder.java
@@ -0,0 +1,7 @@
+package com.bridge.QuotientRemainder;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome, This is a program to compute and print Quotient and Remainder");
+    }
+}
