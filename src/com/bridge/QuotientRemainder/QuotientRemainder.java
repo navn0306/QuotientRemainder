63b4c640ee93f24dd4095d76eee56ac1e0e9feae
Navneet <navn0306@gmail.com>
2021-11-28 01:04:49 +0530

    ADD: Added Java Program to Compute Quotient and Remainder

--- src/com/bridge/QuotientRemainder/QuotientRemainder.java
+++ src/com/bridge/QuotientRemainder/QuotientRemainder.java
@@ -1,7 +1,19 @@
 package com.bridge.QuotientRemainder;
-
+import java.util.*;
 public class QuotientRemainder {
-    public static void main(String[] args) {
-        System.out.println("\n Welcome, This is a program to compute and print Quotient and Remainder");
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of the divisor and divident: ");
+
+        int divisor = sc.nextInt();
+        int divident = sc.nextInt();
+
+        int quotient = divident / divisor;
+        int remainder = divident % divisor;
+
+        System.out.println("Quotient value:" + quotient);
+        System.out.println("Remainder value:" + remainder);
     }
 }
